e61369f5e9900a84c50589afca24b757066f9739
dougpagani <redstonefreedom@gmail.com>
2019-04-12 12:55:28 -0400

    added tests

--- test/index.js
+++ test/index.js
@@ -5,5 +5,7 @@ const x = require('../index.js')
 describe('pad-left should add chars to left', () => {
     it('adds 0s', () => {
         assert.equal(x.padLeft('asdf', 5, 0), "0asdf")
+        assert.equal(x.padLeft('asdf', 5), " asdf")
+        assert.equal(x.padLeft('asdf', 5, "a"), "sasdf")
     });
 });
